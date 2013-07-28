b6004624c45d913f1b9ab11f14e304da81447dbb
Aaron Neyer <aaronneyer@gmail.com>
2013-07-27 19:02:16 -0700

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+QuantifiedLife::Application.config.secret_key_base = 'f955d12fa0065a962bb1a841bb83d789aa5470fa44fac72a405e31214dd6b124f1295b59b744e55b32c84420698893913dd0d8e5602a2fc0402969c166c8cb1e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+QuantifiedLife::Application.config.session_store :cookie_store, key: '_QuantifiedLife_session'
