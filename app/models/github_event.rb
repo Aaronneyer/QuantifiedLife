35ff7d7b384244a3b26771676d3814625bdabfd0
Aaron Neyer <aaronneyer@gmail.com>
2013-08-18 10:20:02 -0700

    basic start of info_string for github events

--- app/models/github_event.rb
+++ app/models/github_event.rb
@@ -1,4 +1,12 @@
 class GithubEvent
   include Mongoid::Document
   include Mongoid::Attributes::Dynamic
+
+  #TODO: These should probably be partials that I render for the timeline.
+  def info_string
+    case type
+    when "CommitCommentEvent"
+      "Commented on #{payload['comment']['commit_id']} with #{payload['comment']['body']}"
+    end
+  end
 end
