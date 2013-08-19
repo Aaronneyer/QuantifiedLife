55bb8ca468c31abf5e38658e25152fb1f8334d22
Aaron Neyer <aaronneyer@gmail.com>
2013-08-19 03:36:01 -0400

    tags and impact wooo

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,8 +7,17 @@ class Post
   slug :title, history: true
   field :body, type: String
   field :date, type: Date
+  field :impact, type: Fixnum
+  field :tags, type: Array, default: []
 
   index({ date: -1 })
+
+  def tags=(new_tags)
+    if new_tags.is_a?(String)
+      new_tags = new_tags.split
+    end
+    super(new_tags)
+  end
 
   def day
     Day.where(date: date).first
