b0b7648a8067d8f2b4b6f1713ed92ec98a61bcb2
Aaron Neyer <aaronneyer@gmail.com>
2013-08-19 03:40:43 -0400

    testing? Never heard of it!

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -70,6 +70,6 @@ class PostsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def post_params
-      params.require(:post).permit(:title, :body, :date)
+      params.require(:post).permit(:title, :body, :date, :impact, :tags)
     end
 end
