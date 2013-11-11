ccf137baf0c25b7845d0ca1763bf708beecc6f8f
Aaron Neyer <aaronneyer@gmail.com>
2013-11-11 07:00:08 -0500

    display headline and markdown summary for days

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,8 +1,2 @@
 module PostsHelper
-  def markdown(text)
-    @markdown ||= Redcarpet::Markdown.new(HTMLwithPygments.new(hard_wrap: true),
-                                          fenced_code_blocks: true,
-                                          lax_html_blocks: true)
-    @markdown.render(text).html_safe
-  end
 end
